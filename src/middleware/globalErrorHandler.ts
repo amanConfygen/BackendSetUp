c57836f48deea18f6ecd22870d95dad7976f35d6
amanComfygen <amancomfygen@gmail.com>
2025-02-11 14:31:29 +0530

    feat: Saved

--- src/middleware/globalErrorHandler.ts
+++ src/middleware/globalErrorHandler.ts
@@ -1,6 +1,7 @@
-import { Request, Response} from 'express';
-import { THttpError } from '../types/types';
+import { NextFunction, Request, Response } from 'express'
+import { THttpError } from '../types/types'
 
-export default (err: THttpError, _: Request, res: Response) => {
-    res.status(err.statusCode).json(err);
-};
+export default (err: THttpError, _: Request, res: Response, __: NextFunction) => {
+    res.status(err.statusCode).json(err)
+}
+
